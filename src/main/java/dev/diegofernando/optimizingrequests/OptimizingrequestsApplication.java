b6a2e59c5e7e2b28caa19812d58b42bf0a0dbab7
Diego Fernando <diegofelima.ti@gmail.com>
2020-09-02 21:24:11 -0300

    first commit

---
+++ src/main/java/dev/diegofernando/optimizingrequests/OptimizingrequestsApplication.java
@@ -0,0 +1,13 @@
+package dev.diegofernando.optimizingrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizingrequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizingrequestsApplication.class, args);
+	}
+
+}
